0814755cbf6996239e81f5718990aab11fa225c2
ADMIN <ADMIN@DESKTOP-H58APAC>
2025-05-12 19:32:48 +0800

    Перерисовал на чёткий кораблик

--- Entities/PlayerShip.cs
+++ Entities/PlayerShip.cs
@@ -6,7 +6,10 @@ namespace SpaceBallCrusher1.Entities
     {
         public float X { get; set; }
         public float Y { get; set; }
-        private const int Size = 30;
+        private const int ShipWidth = 40;
+        private const int ShipHeight = 60;
+        private const int EngineSize = 15;
+        private const int CockpitSize = 20;
 
         public PlayerShip(float x, float y)
         {
@@ -22,15 +25,85 @@ namespace SpaceBallCrusher1.Entities
 
         public void Draw(Graphics g)
         {
-            PointF[] shipPoints = new PointF[3]
+            // Корпус корабля (основной треугольник)
+            PointF[] hull = new PointF[3]
             {
-                new PointF(X, Y - Size),
-                new PointF(X - Size/2, Y + Size/2),
-                new PointF(X + Size/2, Y + Size/2)
+                new PointF(X, Y - ShipHeight/2), // Нос корабля
+                new PointF(X - ShipWidth/2, Y + ShipHeight/2), // Левая задняя точка
+                new PointF(X + ShipWidth/2, Y + ShipHeight/2)  // Правая задняя точка
             };
-            
-            g.FillPolygon(Brushes.LightBlue, shipPoints);
-            g.DrawPolygon(Pens.White, shipPoints);
+
+            // Кабина (полукруг)
+            RectangleF cockpitRect = new RectangleF(
+                X - CockpitSize / 2,
+                Y - ShipHeight / 2 - CockpitSize / 3,
+                CockpitSize,
+                CockpitSize);
+
+            // Двигатели (задние прямоугольники)
+            RectangleF leftEngine = new RectangleF(
+                X - ShipWidth / 3,
+                Y + ShipHeight / 2,
+                EngineSize / 2,
+                EngineSize);
+
+            RectangleF rightEngine = new RectangleF(
+                X + ShipWidth / 3 - EngineSize / 2,
+                Y + ShipHeight / 2,
+                EngineSize / 2,
+                EngineSize);
+
+            // Центральный двигатель
+            RectangleF centerEngine = new RectangleF(
+                X - EngineSize / 3,
+                Y + ShipHeight / 2,
+                EngineSize * 2 / 3,
+                EngineSize);
+
+            // Рисуем корпус
+            g.FillPolygon(new SolidBrush(Color.FromArgb(70, 130, 180)), hull); // Стальной синий
+            g.DrawPolygon(Pens.White, hull);
+
+            // Рисуем кабину
+            g.FillEllipse(new SolidBrush(Color.FromArgb(100, 149, 237)), cockpitRect); // Голубой
+            g.DrawEllipse(Pens.White, cockpitRect);
+
+            // Рисуем двигатели
+            g.FillRectangle(Brushes.DarkSlateGray, leftEngine);
+            g.FillRectangle(Brushes.DarkSlateGray, rightEngine);
+            g.FillRectangle(Brushes.DarkSlateGray, centerEngine);
+            g.DrawRectangle(Pens.White, leftEngine.X, leftEngine.Y, leftEngine.Width, leftEngine.Height);
+            g.DrawRectangle(Pens.White, rightEngine.X, rightEngine.Y, rightEngine.Width, rightEngine.Height);
+            g.DrawRectangle(Pens.White, centerEngine.X, centerEngine.Y, centerEngine.Width, centerEngine.Height);
+
+            // Огни двигателей (анимация)
+            if (DateTime.Now.Millisecond % 500 < 250)
+            {
+                PointF[] leftFlame = new PointF[3]
+                {
+                    new PointF(X - ShipWidth/3 + EngineSize/4, Y + ShipHeight/2 + EngineSize),
+                    new PointF(X - ShipWidth/3, Y + ShipHeight/2 + EngineSize + 10),
+                    new PointF(X - ShipWidth/3 + EngineSize/2, Y + ShipHeight/2 + EngineSize + 10)
+                };
+
+                PointF[] rightFlame = new PointF[3]
+                {
+                    new PointF(X + ShipWidth/3 - EngineSize/4, Y + ShipHeight/2 + EngineSize),
+                    new PointF(X + ShipWidth/3 - EngineSize/2, Y + ShipHeight/2 + EngineSize + 10),
+                    new PointF(X + ShipWidth/3, Y + ShipHeight/2 + EngineSize + 10)
+                };
+
+                PointF[] centerFlame = new PointF[3]
+                {
+                    new PointF(X, Y + ShipHeight/2 + EngineSize),
+                    new PointF(X - EngineSize/3, Y + ShipHeight/2 + EngineSize + 15),
+                    new PointF(X + EngineSize/3, Y + ShipHeight/2 + EngineSize + 15)
+                };
+
+                g.FillPolygon(Brushes.OrangeRed, leftFlame);
+                g.FillPolygon(Brushes.OrangeRed, rightFlame);
+                g.FillPolygon(Brushes.OrangeRed, centerFlame);
+            }
         }
     }
 }
